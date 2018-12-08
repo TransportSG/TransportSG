394a5c55aecceceaea47d192569d96c2e749a68a
Eyeball Edward <edwardyeung.private@gmail.com>
2018-12-08 22:03:03 +1100

    remove config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    useHTTPS: false,
-    sslCertPath: '',
-    httpPort: 8000,
-    databaseURL: 'mongodb://localhost:27017',
-    websiteDNSName: 'transportsg.me',
-    webrootPath: ''
-}
