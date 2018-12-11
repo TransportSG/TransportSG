628ae64b906d06d433820502c331d14b9133e4e5
Eyeball Edward <edwardyeung.private@gmail.com>
2018-12-11 21:34:46 +1100

    better dest line solving

--- application/routes/MRTTimingsRouter.js
+++ application/routes/MRTTimingsRouter.js
@@ -23,12 +23,55 @@ function findStationNumber(line, stationName) {
     return number;
 }
 
+function findStationLines(stationName) {
+    let lines = [];
+    Object.keys(mrtLines).forEach(lineName => {
+        let mrtLine = mrtLines[lineName];
+
+        mrtLine.stations.forEach((stn, i) => {
+            if (stn.stationName === stationName) {
+                lines.push(lineName);
+            }
+        });
+    });
+
+    return lines;
+}
+
+function resolveDestinationLines(lineName, timings) {
+    function abbv(line) {
+        let abbvLine = lineName.split(' ').map(p=>p.slice(0, 1)).join('');
+        if (abbvLine === 'CL') abbvLine = 'CCL';
+
+        return abbvLine;
+    }
+
+    let dests = {};
+
+    Object.keys(timings).forEach(currentLineName => {
+        Object.keys(timings[currentLineName]).forEach(destinationName => {
+            let possibleLines = findStationLines(destinationName);
+
+            let biased = possibleLines.filter(possible => abbv(possible) == currentLineName);
+            if (biased.length === 1) {
+                dests[destinationName] = possibleLines.filter(possible => abbv(possible) == currentLineName)[0];
+            } else {
+                dests[destinationName] = possibleLines[0];
+            }
+        });
+    });
+
+    return dests;
+}
+
 router.get('/:lineName/:stationName', (req, res) => {
     let {lineName, stationName} = req.params;
 
     getMRTTimings(lineName, stationName, timings => {
+        let resolvedDests = resolveDestinationLines(lineName, timings);
+
         res.render('mrt/timings/results',
-            {lineName, stationName, stationNumber: findStationNumber(lineName, stationName), timings: timings});
+            {lineName, resolvedDests, stationName, stationNumber: findStationNumber(lineName, stationName), timings: timings});
     });
 });
 
