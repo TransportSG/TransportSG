32b552bfd5f05eadfbf0820e1bef3bffdaeb1ff8
Eyeball Edward <edwardyeung.private@gmail.com>
2018-12-20 12:05:47 +1100

    redesigned

--- application/static/scripts/bus/timings-list.js
+++ application/static/scripts/bus/timings-list.js
@@ -1,3 +1,5 @@
+let currentlyShowing = null;
+
 function performQuery() {
     let query = $('#input').value;
     if (query.trim() ==  '') return;
@@ -16,14 +18,35 @@ function performQuery() {
         else
             $('#results').innerHTML = content;
 
-        let currentlyShowing = null;
         createDropdown('service-selector', service => {
-            if (currentlyShowing !== null) {console.log(currentlyShowing)
-                $(`#busServiceContainer-${currentlyShowing}`).style.display = 'none'}
+            if (currentlyShowing)
+                $(`#busServiceContainer-${currentlyShowing}`).style.display = 'none'
             $(`#busServiceContainer-${service}`).style.display = 'flex';
 
             currentlyShowing = service;
+
+            let parent = $(`#busServiceContainer-${service}`);
+            if (parent.className.endsWith('1')) {
+                $('#dir-2').style.display = 'none';
+            } else {
+                $('#dir-2').style.display = 'flex';
+            }
         });
+
+        let currentDirection = 1;
+        function setDirection(direction) {
+            let width = window.innerWidth;
+            if (width > 700) return;
+
+            $(`#dircontainer-${currentlyShowing}-${currentDirection}`).style.display = 'none';
+            $(`#dircontainer-${currentlyShowing}-${direction}`).style.display = 'block';
+
+            currentDirection = direction;
+        }
+
+        $('#dir-1').on('click', setDirection.bind(null, 1));
+        $('#dir-2').on('click', setDirection.bind(null, 2));
+
     });
 }
 
@@ -44,4 +67,17 @@ $.ready(() => {
     }
 
     performQuery();
+
+    window.on('resize', () => {
+        let width = window.innerWidth;
+        if (width > 700) {
+            if ($('#dir-1')) {
+                let parent = $(`#busServiceContainer-${currentlyShowing}`);
+                if (parent.className.endsWith('2')) {
+                    $(`#dircontainer-${currentlyShowing}-2`).style.display = 'block';
+                }
+                $(`#dircontainer-${currentlyShowing}-1`).style.display = 'block';
+            }
+        }
+    });
 });
