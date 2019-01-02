454d73601b0d82259425b7df358bd13dbdc8b0df
Eyeball Edward <edwardyeung.private@gmail.com>
2019-01-02 11:27:16 +1100

    auto updating times

--- application/static/scripts/bus/candies.js
+++ application/static/scripts/bus/candies.js
@@ -129,3 +129,31 @@ $.ready(() => {
 
     window.on('resize', updateDisplay);
 });
+
+function getTimingsDifference(a, b) {
+    let d = new Date(Math.abs(a - b));
+    return {minutes: d.getUTCMinutes(),seconds: d.getUTCSeconds()};
+};
+function hasArrived(timing) {
+    return +new Date() - +new Date(timing) > 0;
+}
+
+function setTimes() {
+    let now = new Date();
+
+    Array.from(document.querySelectorAll('.bus-arrival')).forEach(div => {
+        let arrivalTime = new Date(div.getAttribute('arrival'));
+        let difference = getTimingsDifference(arrivalTime, now);
+
+        if (hasArrived(arrivalTime)) {
+            div.children[0].textContent = 'Arr';
+            div.children[0].className = 'bus-arr';
+            div.children[1].textContent = '';
+        } else {
+            div.children[0].textContent = difference.minutes;
+            div.children[1].textContent = difference.seconds;
+        }
+    });
+}
+
+setInterval(setTimes, 1000);
