31110f18786be01e85dcb833f65215bd71d8ca10
Eyeball Edward <edwardyeung.private@gmail.com>
2019-01-13 00:25:09 +1100

    better searching

--- application/secret/nothing_to_see_here/go_away/present_sleigh.js
+++ application/secret/nothing_to_see_here/go_away/present_sleigh.js
@@ -63,9 +63,11 @@ function createEmailBody(callback) {
         kjdepUpsize: getServices('307 DD'),
         kjdepDownsize: getServices('180 972 SD'),
         kjdepBendy: getServices('61 176 180 700 700A 972 983 985 BD'),
-        woodlandsBendy: getServices('960e 961 963 965 966 178 187 BD'),
+        woodlandsBendy: getServices('178 187 960e 961 963 965 966 951e BD'),
         slbpDownsize: getServices('179 179A 182 192 198 198A 241 247 248 249 251 252 253 254 255 257 SD'),
-        bndepDownsize: getServices('23 65 7A SD'),
+        bndepDownsize: getServices('23 35 35M 37 65 168 7A SD'),
+        hgdepDownsize: getServices('87 89e 161 SD'),
+        brbpDownsize: getServices('88 SD'),
         sedepBendy: getServices('SEDEP !800 !804 !806 !807 !811 !812 BD'),
         updepUpsize: getServices('120 122 272 273 93 DD'),
         bbdepDownsize: getServices('147e 147 7B SD')
